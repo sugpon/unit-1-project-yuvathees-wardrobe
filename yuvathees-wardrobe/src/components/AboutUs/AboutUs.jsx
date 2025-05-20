f39f9eae244b8627d8b6ae74331e52c7b1cc0252
Suganya Ponnusamy <suganya.sugu88@gmail.com>
2025-05-20 14:27:50 -0500

    Initial component structure and skeleton setup

---
+++ yuvathees-wardrobe/src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <section>
+      <h2>About Page</h2>
+      {/* Placeholder content */}
+      <p>This is the About page of Yuvathees Wardrobe.</p>
+    </section>
+  );
+};
+
+export default AboutUs;
---
+++ yuvathees-wardrobe/src/components/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <section>
+      <h2>About Page</h2>
+      {/* Placeholder content */}
+      <p>This is the About page of Yuvathees Wardrobe.</p>
+    </section>
+  );
+};
+
+export default Services;
